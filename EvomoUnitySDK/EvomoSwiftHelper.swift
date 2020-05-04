2bc4d051239136dad70f872aa9dc623a0ea11478
Jakob Wowy <jakob.wowy@evomo.de>
2020-05-04 19:09:24 +0200

    Release 1.0.5.

--- EvomoUnitySDK/EvomoSwiftHelper.swift
+++ EvomoUnitySDK/EvomoSwiftHelper.swift
@@ -9,53 +9,92 @@
 import Foundation
 import EvomoMotionAI
 
+let deviceIphone = Device(deviceID: "", deviceType: .iPhone, devicePosition: .hand,
+                          deviceOrientation: .buttonDown, classificationModel: "2102")
+
+let licenseID = "800ff7ea-521b-4d5f-b1f9-c04e90d665fa"
 
 @objc public class EvomoSwiftHelper: NSObject {
     
     @objc public static func initEvomo(unityBridge : EvomounityBridge) {
         
+        let unityBridge: EvomounityBridge = EvomounityBridge()
+        
+        var lastElmo: ElementalMovement? = nil
+        
         ClassificationControlLayer.shared.elementalMovementHandler = { elementalMovement in
-            if !elementalMovement.rejected {
-                let gameEvent: String?
-                switch(elementalMovement.typeLabel) {
-                case "hop_single_straight_up":
-                    gameEvent = "jump"
-                    unityBridge.jump()
-                case "duck down":
-                    gameEvent = "duck"
-                    unityBridge.duck()
-                case "side_step_left_up":
-                    gameEvent = "left"
-                    unityBridge.left()
-                case "side_step_right_up":
-                    gameEvent = "right"
-                    unityBridge.right()
-                default:
-                    gameEvent = nil
-                }
-                if gameEvent != nil {
-                    print(gameEvent! + " ID:" + String( elementalMovement.typeID))
+            // Will be executed every time a elementalMovement was classified
+            
+            DispatchQueue.main.async {
+                // execute movement event in main thread
+                if !elementalMovement.rejected {
+                    print("EvomoMovement: \(elementalMovement.typeLabel)")
+                    
+                    switch(elementalMovement.typeLabel) {
+                    case "hop_single_up":
+                        unityBridge.jump()
+                    case "duck down":
+                        unityBridge.duck()
+                    case "side_step_left_up":
+                        unityBridge.left()
+                    case "side_step_right_up":
+                        unityBridge.right()
+                    default:
+                        print("default")
+                    }
+                    
+                    // rescue rejected elmo
+                    if let lastElmo = lastElmo {
+                        if lastElmo.rejected == true && elementalMovement.rejected == false {
+                            if lastElmo.typeLabel == "duck down" && elementalMovement.typeLabel == "duck up"  {
+                                // rescue hop
+                                unityBridge.duck()
+                            } else if lastElmo.typeLabel == "hop_single_up" && elementalMovement.typeLabel == "hop_single_down"  {
+                                // rescue hop
+                                unityBridge.jump()
+                            } else if lastElmo.typeLabel == "side_step_left_up" && elementalMovement.typeLabel == "side_step_left_down"  {
+                                // rescue hop
+                                unityBridge.left()
+                            } else if lastElmo.typeLabel == "side_step_right_up" && elementalMovement.typeLabel == "side_step_right_down"  {
+                                // rescue hop
+                                unityBridge.right()
+                            }
+                        }
+                    }
                 }
             }
-        }        
+            lastElmo = elementalMovement
+        }
+        
+        
+        ClassificationControlLayer.shared.debugging = true
+        ClassificationControlLayer.shared.gaming = true
+        
+        // only used to load model on init
+        ClassificationControlLayer.shared.setLicense(licenseID: licenseID)
+        ClassificationControlLayer.shared.getAvailableMovements(device: deviceIphone).done{ mTypes in
+            print("Evomo - getAvailableMovements", mTypes)
+        }.catch{ error in
+            print("Evomo - getAvailableMovements", error)
+        }
+        
     }
     
     @objc public static func startEvomo() {
-       let deviceIphone = Device(deviceID: "", deviceType: .iPhone, devicePosition: .hand, deviceOrientation: .buttonDown,
-                                 classificationModel: "2057")
-       
-        ClassificationControlLayer.shared.gaming = true
-
-        ClassificationControlLayer.shared.start(
-              devices: [deviceIphone],
-              licenseID: "143ab59d-c267-4dde-aa0d-c2314df72e81",
-              isStarted: {
-                  print("Evomo Startead!")
-                  
-          },
-              isFailed: {error in print("\(error)")}
-          )
-   }
+        // Define device
+                                      
+            print("Start classification")
+                  // Start
+                  ClassificationControlLayer.shared.start(
+                      devices: [deviceIphone],
+                      licenseID: licenseID,
+                      isStarted: {
+                          print("Evomo - Started!")
+                          
+                  },
+                      isFailed: {error in print("Evomo - startClassification:  \(error)")}
+                  )
+       }
     
     @objc public static func stopEvomo() {
         ClassificationControlLayer.shared.stop()
